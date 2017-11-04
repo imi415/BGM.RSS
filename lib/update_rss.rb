9e5e84d527dced9627fc62e0987bea8d7a3f66dd
imi415 <imi415@imi.moe>
2017-11-04 16:20:12 +0800

    Added torrent cache.

--- lib/update_rss.rb
+++ lib/update_rss.rb
@@ -4,6 +4,9 @@ Feed.all.each do | feed |
   # Each feed
   rss = RSS::Parser.parse(feed.url, true)
 
+  torrent_path = '/var/www/html/torrents'
+  torrent_url = 'https://bgm.imi.moe/torrents'
+
   torrent_filter = /bangumi\.moe/
   is_torrent = false
   if torrent_filter.match(feed.url) then
@@ -11,17 +14,24 @@ Feed.all.each do | feed |
     is_torrent = true
   end
 
+  #  Update last_updated
+  feed.last_updated = rss.items.first.pubDate
+  feed.save
+
   # Each torrent
   rss.items.each_with_index do | item, index |
 
     ih = ""
 
     if is_torrent then
-      %x(curl --globoff -s "#{URI.encode(item.enclosure.url)}" --output "/tmp/#{item.title}.torrent")
-      File.open("/tmp/#{item.title}.torrent", "r") do | f |
-        str = f.read
-        ih =  OpenSSL::Digest::SHA1.hexdigest(BEncode.load(str)["info"].bencode)
+      if !File.exist?("#{torrent_path}/#{item.title}.torrent") then
+        %x(curl --globoff -s "#{URI.encode(item.enclosure.url)}" --output "#{torrent_path}/#{item.title}.torrent")
+        File.open("#{torrent_path}/#{item.title}.torrent", "r") do | f |
+          str = f.read
+          ih =  OpenSSL::Digest::SHA1.hexdigest(BEncode.load(str)["info"].bencode)
+        end
       end
+      item.enclosure.url = "#{torrent_url}/#{item.title}.torrent"
     else
       magnet_ih_filter = /(magnet:\?xt=urn:btih:).*(\&dn)/
       ih = BaseX::RFC4648Base32.string_to_integer(magnet_ih_filter.match(item.enclosure.url)[0].slice(20, 32)).to_s(16)
@@ -40,8 +50,6 @@ Feed.all.each do | feed |
       i.url = item.enclosure.url
       i.feed_id = feed.id
       i.status = 'PENDING_CREATE'
-      feed.last_updated = item.pubDate
-      feed.save
       i.save
     else # unless
       i.ep_id = rss.items.length - index
