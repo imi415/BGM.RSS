de4d1713ecc843206155bf5b09e7dcaa04fa201e
imi415.public@gmail.com <imi415.public@gmail.com>
2017-11-06 11:20:33 +0800

    Catch slice exceptions

--- lib/perform_slice.rb
+++ lib/perform_slice.rb
@@ -11,40 +11,44 @@ client = TransmissionApi::Client.new(
 slice_dir = '/var/www/html/hls'
 
 unless (status == 'SLICING')
-  puts "Idle now, starting job."
-  redis.set("slice_status", 'SLICING')
-  path = client.config_get['download-dir']
-  path += '/'
-  p path
+  begin
+    puts "Idle now, starting job."
+    redis.set("slice_status", 'SLICING')
+    path = client.config_get['download-dir']
+    path += '/'
+    p path
 
-  item = Item.where(:status => 'PENDING_SLICE').first
-  if (item)
-    p item.id
-    task = client.find(item.taskid)
-    path += task['name']
-    unless task['files'].length == 1 then
-      task['files'].each do | file |
-        media_file_filter = /(\.mp4$)|(\.mkv$)|(\.ts$)/
-        if media_file_filter.match(file['name'])
-          path += file['name']
-          break
+    item = Item.where(:status => 'PENDING_SLICE').first
+    if (item)
+      p item.id
+      task = client.find(item.taskid)
+      path += task['name']
+      unless task['files'].length == 1 then
+        task['files'].each do | file |
+          media_file_filter = /(\.mp4$)|(\.mkv$)|(\.ts$)/
+          if media_file_filter.match(file['name'])
+            path += file['name']
+            break
+          end
         end
       end
-    end
-    item.status = 'SLICING'
-    item.save
+      item.status = 'SLICING'
+      item.save
 
-    vid = FFMPEG::Movie.new("#{path}")
-    if vid.video_codec == 'h264' then
-      %x(#{File.dirname(__FILE__)}/slice_only.sh "#{path}" #{slice_dir}/#{item.id})
-    else
-      %x(#{File.dirname(__FILE__)}/slice_convert.sh "#{path}" #{slice_dir}/#{item.id} #{vid.rsolution})
+      vid = FFMPEG::Movie.new("#{path}")
+      if vid.video_codec == 'h264' then
+        %x(#{File.dirname(__FILE__)}/slice_only.sh "#{path}" #{slice_dir}/#{item.id})
+      else
+        %x(#{File.dirname(__FILE__)}/slice_convert.sh "#{path}" #{slice_dir}/#{item.id} #{vid.rsolution})
+      end
+      item.status = 'AVAILABLE'
+      item.save
     end
-    item.status = 'AVAILABLE'
-    item.save
+    redis.set("slice_status", 'IDLE')
+    puts "Job done."
+  rescue Exception  => e
+    redis.set("slice_status", 'IDLE')
   end
-  redis.set("slice_status", 'IDLE')
-  puts "Job done."
 else
   puts "Busy now, waiting for next slot."
 end
