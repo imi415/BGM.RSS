c228a0700f57b5bdd444611a8041d767aeefa171
imi415.public@gmail.com <imi415.public@gmail.com>
2017-11-20 10:16:38 +0800

    Updated gems and fixed tooltip with turbolinks.

--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -1,6 +1,15 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+//$(function () {
+//  $('[data-toggle="popover"]').popover()
+//})
+//$(function () {
+//  $('[data-toggle="tooltip"]').tooltip()
+//})
+
+$(document).on("ready page:change", function() {
+  $("[data-toggle='tooltip']")
+    .tooltip("destroy")
+    .tooltip();
+  $('[data-toggle="popover"]')
+    .popover("destroy")
+    .popover();
+});
