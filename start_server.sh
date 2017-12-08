95f06154095829dca62b7c4c18b606675ec8e272
imi415.public@gmail.com <imi415.public@gmail.com>
2017-12-08 22:56:30 +0800

    Add secret.

--- start_server.sh
+++ start_server.sh
@@ -4,6 +4,10 @@ source .env
 
 echo $RAILS_ENV
 
+if [ $RAILS_ENV -eq "production" ]; then
+	export SECRET_KEY_BASE=`bin/rake secret`
+fi
+
 echo $TRANSMISSION_URL
 echo $REDIS_HOST
 echo $SLICE_DIR
